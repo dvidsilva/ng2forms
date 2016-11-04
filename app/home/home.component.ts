d97b26fb3fe7204f3cbc243a12e4309bb23b9dfc
David Silva <contacto@dvidsilva.com>
2016-11-04 12:51:13 -0700

    added property and event binding examples to home.component

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -3,10 +3,27 @@ import { Component } from '@angular/core';
 
 @Component({
     template: `<section>
+        <h2>{{welcome_message}}</h2>
         <p>Workshop prepared for <a href="https://qconsf.com/sf2016/workshop/angular-2-forms-workshop" target="_blank">QConSF</a></p>
         <p>Find the repo for this code in <a href="https://github.com/dvidsilva/ng2forms">GitHub</a></p>
         <p>A live version of this project maybe is found in 
-            <a href="https://dvidsilva.github.io/ng2forms">dvidsilva.github.io/ng2forms</a></p>      
-    </section>`
+            <a href="https://dvidsilva.github.io/ng2forms">dvidsilva.github.io/ng2forms</a></p>
+        <p><button (click)="showAlert()">Click here to see an alert</button></p>
+        <p><strong>This is a two way data binding with ngModel:</strong>
+        <input [(ngModel)]="welcome_message" ></p>
+    </section>`,
+    styles: [`
+    input {
+        font-size: 2em;
+        padding: 4px;
+        display: block;
+     }
+    `]
 })
-export class HomeComponent { }
+export class HomeComponent { 
+  welcome_message = 'Welcome to NG2Forms Workshop';
+
+  showAlert() {
+    alert(this.welcome_message);
+  }
+}
