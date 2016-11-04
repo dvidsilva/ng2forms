79dde88f2dc59602872a7c1c2ff494e12fddda44
David Silva <contacto@dvidsilva.com>
2016-11-03 17:30:10 -0700

    created a home component

---
+++ app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'home',
+    template: `
+    <p>Workshop prepared for <a href="https://qconsf.com/sf2016/workshop/angular-2-forms-workshop"
+        target="_blank">QConSF</a></p>
+    <p>Find the repo for this code in <a href="https://github.com/dvidsilva/ng2forms">GitHub</a></p>
+    <p>A live version of this project can be found in 
+    <a href="https://dvidsilva.github.io/ng2forms>dvidsilva.github.io/ng2forms</a></p>`
+})
+export class HomeComponent { }
