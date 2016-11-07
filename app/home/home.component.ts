7d7dacbb7ecf482632925728e532ccb2ca38ba7e
David Silva <contacto@dvidsilva.com>
2016-11-07 11:11:08 -0800

    added ng material componetns to homepage

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -3,18 +3,20 @@ import { Component } from '@angular/core';
 
 @Component({
     template: `<section>
-        <h2>{{welcome_message}}</h2>
+        <md-card>
+            <md-card-title>{{welcome_message}}</md-card-title>
         <p>Workshop prepared for <a href="https://qconsf.com/sf2016/workshop/angular-2-forms-workshop" target="_blank">QConSF</a></p>
         <p>Find the repo for this code in <a href="https://github.com/dvidsilva/ng2forms">GitHub</a></p>
-        <p>A live version of this project maybe is found in 
-            <a href="https://dvidsilva.github.io/ng2forms">dvidsilva.github.io/ng2forms</a></p>
-        <p><button (click)="showAlert()">Click here to see an alert</button></p>
+        <!--<p>A live version of this project maybe is found in 
+            <a href="https://dvidsilva.github.io/ng2forms">dvidsilva.github.io/ng2forms</a></p>-->
+        <p><button md-raised-button (click)="showAlert()">Click here to see an alert</button></p>
         <p><strong>This is a two way data binding with ngModel:</strong>
         <input [(ngModel)]="welcome_message" ></p>
         <p>
             <label> Local variable: {{phone_number}}</label>
             <input #phone placeholder="local variable" (change)="setLocalVariable(phone.value)" (keyup)="setLocalVariable(phone.value)">
        </p>
+       </md-card>
     </section>`,
     styles: [`
     input {
